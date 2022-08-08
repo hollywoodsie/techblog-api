28cc67fbf90d0c549a8d46c39f7b48ab1b848b54
hollywoodsie <kovalioff28@gmail.com>
2022-08-09 01:23:46 +0300

    deploy

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/', router);
+app.use('/api', router);
 
 app.listen(port, () => console.log(`Server started on ${port}`));
